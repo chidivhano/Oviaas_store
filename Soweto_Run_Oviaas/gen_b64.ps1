34027a6747e3da86f89a4e7fb8ffaddccbf0764c
Dagada <dagadr@unisa.ac.za>
2026-03-30 15:33:38 +0200

    Update Soweto Run game and integration path

--- Soweto_Run_Oviaas/gen_b64.ps1
+++
@@ -1,3 +0,0 @@
-$bytes = [System.IO.File]::ReadAllBytes('c:\Users\dagadr\Downloads\Soweto_Run_Oviaas\assets\Oviaas_Coin.png')
-$b64 = [System.Convert]::ToBase64String($bytes)
-[System.IO.File]::WriteAllText('c:\Users\dagadr\Downloads\Soweto_Run_Oviaas\b64.txt', $b64)
